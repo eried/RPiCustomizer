7b56a8b4a0355111298e982d8234d08d23059f6c
Erwin Ried <erwinried@gmail.com>
2014-03-24 20:17:54 -0300

    Little tweaks in the IniParser

--- RPiCustomizer/IniParser.cs
+++ RPiCustomizer/IniParser.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Collections;
 using System.Text;
 using RPICustomizer.Properties;
 
-namespace CarPlateMonitor
+namespace RPICustomizer
 {
     /// <summary>
     /// Source: http://bytes.com/topic/net/insights/797169-reading-parsing-ini-file-c
@@ -25,7 +25,7 @@ namespace CarPlateMonitor
         /// Opens the INI file at the given path and enumerates the values in the IniParser.
         /// </summary>
         /// <param name="iniPath">Full path to INI file.</param>
-        public IniParser(String iniPath)
+        public IniParser(String iniPath, bool isContents)
         {
             TextReader iniFile = null;
             String strLine = null;
@@ -34,60 +34,71 @@ namespace CarPlateMonitor
 
             _iniFilePath = iniPath;
 
-            if (File.Exists(iniPath))
+            if (isContents || !File.Exists(iniPath))
             {
-                try
-                {
-                    iniFile = new StreamReader(iniPath, Encoding.Default);
-
-                    strLine = iniFile.ReadLine();
-
-                    while (strLine != null)
-                    {
-                        strLine = strLine.Trim();
-
-                        if (strLine != "")
-                        {
-                            if (strLine.StartsWith("[") && strLine.EndsWith("]"))
-                            {
-                                currentRoot = strLine.Substring(1, strLine.Length - 2);
-                            }
-                            else
-                            {
-                                keyPair = strLine.Split(new char[] { '=' }, 2);
-
-                                SectionPair sectionPair;
-                                String value = null;
-
-                                if (currentRoot == null)
-                                    currentRoot = Resources.IniParser_RootSection;
-
-                                sectionPair.Section = currentRoot;
-                                sectionPair.Key = keyPair[0];
-
-                                if (keyPair.Length > 1)
-                                    value = keyPair[1];
-
-                                _keyPairs.Add(sectionPair, value);
-                            }
-                        }
-
-                        strLine = iniFile.ReadLine();
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-                finally
-                {
-                    if (iniFile != null)
-                        iniFile.Close();
-                }
+                iniFile = new StringReader((String) iniPath.Clone());
+                iniPath = Path.GetTempFileName();
+                ConfigurationFile = iniPath;
             }
             else
-                throw new FileNotFoundException("Unable to locate " + iniPath);
+            {
+                iniFile = new StreamReader(iniPath, Encoding.Default);
+                ConfigurationFile = iniPath;
+            }
+
+            try
+            {
+                strLine = iniFile.ReadLine();
+
+                while (strLine != null)
+                {
+                    strLine = strLine.Trim();
+
+                    if (strLine != "")
+                    {
+                        if (strLine.StartsWith("[") && strLine.EndsWith("]"))
+                        {
+                            currentRoot = strLine.Substring(1, strLine.Length - 2);
+                        }
+                        else
+                        {
+                            keyPair = strLine.Split(new char[] {'='}, 2);
+
+                            SectionPair sectionPair;
+                            String value = null;
+
+                            if (currentRoot == null)
+                                currentRoot = Resources.IniParser_RootSection;
+
+                            sectionPair.Section = currentRoot;
+                            sectionPair.Key = keyPair[0];
+
+                            if (keyPair.Length > 1)
+                                value = keyPair[1];
+
+                            if (_keyPairs.ContainsKey(sectionPair))
+                                _keyPairs[sectionPair] = value;
+                            else
+                                _keyPairs.Add(sectionPair, value);
+                        }
+                    }
+
+                    strLine = iniFile.ReadLine();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (iniFile != null)
+                    iniFile.Close();
+            }
+            /*}
+            else
+                throw new FileNotFoundException("Unable to locate " + iniPath);*/
 
         }
 
@@ -227,7 +238,9 @@ namespace CarPlateMonitor
         /// </summary>
         public void SaveSettings()
         {
-            SaveSettings(_iniFilePath);
+            SaveSettings(ConfigurationFile);
         }
+
+        public string ConfigurationFile { get; set; }
     }
 }
